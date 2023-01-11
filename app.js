68d1f4761e1054ea02052ebd3165c058e1c389a1
EC2 Default User <ec2-user@ip-172-31-38-76.ap-northeast-1.compute.internal>
2023-01-11 07:01:36 +0000

    2023/01/11

---
+++ app.js
@@ -0,0 +1,9 @@
+/*$(document).ready(function (){
+  $('body').html('<h1>Hello jQuery!!</h1>');
+}); */
+
+$(function(){
+  $('.box1').mouseover(function(){
+   $('.box1').css({'background-color':'#0000ff'}); 
+  });
+});
